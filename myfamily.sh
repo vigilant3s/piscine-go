833ee09020302bee8c94e7abfe268056d1c25092
dgalanis <galaisdimitrios84@gmail.com>
2025-05-05 16:42:39 +0300

    My family

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 curl -s https://platform.zone01.gr/assets/superhero/all.json | \
-jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives | if type == "array" then join(", ") else . end'
+jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives | 
+  if type == "array" then join(", ") 
+  elif type == "string" then . 
+  else "No relatives listed" 
+  end'
