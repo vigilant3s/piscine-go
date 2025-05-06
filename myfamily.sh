11c9643b768b2bcd1727305dcddb455dbe6595c6
dgalanis <galanisdimitrios84@gmail.com>
2025-05-06 13:23:24 +0300

    My family

--- myfamily.sh
+++ myfamily.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 curl -s https://platform.zone01.gr/assets/superhero/all.json | \
-jq -r --arg id "$HERO_ID" '
-  .[] | select(.id == ($id | tonumber)) | .connections.relatives
-' | tr '\n' ';' | sed 's/;$//'
+jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives' | \
+tr '\n' ';' | sed 's/;$/\n/'
