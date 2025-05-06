46e804e7e148ff5e286151c0d71f42c1e345d898
dgalanis <galanisdimitrios84@gmail.com>
2025-05-06 14:40:50 +0300

    My family

--- myfamily.sh
+++ myfamily.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Fetch the superhero data and process it
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r ".[] | select(.id == $HERO_ID) | .connections.relatives" | tr '\n' ' '
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq ".[] | select(.id == $HERO_ID) | .connections.relatives" | tr -d '"'
