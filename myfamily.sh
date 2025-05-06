c99938a02414f87923a717832f83c15f7099533d
dgalanis <galanisdimitrios84@gmail.com>
2025-05-06 13:44:14 +0300

    My family

--- myfamily.sh
+++ myfamily.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-
 # Fetch the superhero data and process it
-
 curl -s https://platform.zone01.gr/assets/superhero/all.json | \
-
 jq -r --arg id "$HERO_ID" '
-
   .[] | 
-
   select(.id == ($id | tonumber)) | 
-
   .connections.relatives | 
-
   gsub("\""; "") | 
-
-  gsub("\n"; "")' | \
-
+  gsub("\n"; " ") |  # Replace newlines with spaces
+  gsub(" +"; " ")     # Replace multiple spaces with a single space
+' | \
 tr '\n' ';' | \
-
 sed 's/;$/\n/'
