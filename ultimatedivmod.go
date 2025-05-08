482f42bfc63a21d29e0830f07c77dbc4c45e7eb1
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 14:05:36 +0300

    New ultimatedivmod.go file with orinalA value

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -2,8 +2,10 @@ package student
 
 func UltimateDivMod(a *int, b *int) {
 	if b != nil && *b != 0 { // Ensure b is not nil and not zero
-		*a = *a / *b
-		*b = *a % *b
+		originalA := *a
+		*a = originalA / *b
+		remainder := originalA % *b
+		*b = remainder
 	} else {
 		// Handle division by zero and set a and b to zero
 		*a = 0
