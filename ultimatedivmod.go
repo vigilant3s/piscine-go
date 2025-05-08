5434c840550188bf0ded9a0b75db8ecb0f3ed71d
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 13:57:53 +0300

    Adding ultimatedivmod.go correction 2

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,12 +1,11 @@
 package student
 
-func DivMod(a int, b int, div *int, mod *int) {
-	if b != 0 {
-		*div = a / b // Perform division if b is not 0
-		*mod = a % b // Perform modulo if b is not 0
+func UltimateDivMod(a *int, b *int) {
+	if *b != 0 {
+		*a = *a / *b
+		*b = *a % *b
 	} else {
-		// Handle the case where division by zero occurs
-		*div = 0 // Set the result of div to 0
-		*mod = 0 // Set the result of mod to 0
+		*a = 0
+		*b = 0
 	}
 }
