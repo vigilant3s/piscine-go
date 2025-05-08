dfe5bc085b83ab343ce244e9794a82c7620e5cd6
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 13:34:41 +0300

    Fixed version of printcombn.go

--- printcombn.go
+++ printcombn.go
@@ -7,8 +7,8 @@ func PrintCombN(n int) {
 		return
 	}
 
-	var combination [10]int
-	generateComb(combination, 0, 0, n)
+	var comb [10]int
+	generateComb(comb, 0, 0, n)
 }
 
 func generateComb(comb [10]int, index, start, n int) {
@@ -16,14 +16,18 @@ func generateComb(comb [10]int, index, start, n int) {
 		for i := 0; i < n; i++ {
 			z01.PrintRune(rune(comb[i] + '0'))
 		}
-		if comb[0] != 10-n { // Don't print comma after the last combo
+		if comb[0] != 10-n {
 			z01.PrintRune(',')
 			z01.PrintRune(' ')
 		}
 		return
 	}
 
-	for digit := start; digit <= 9-(n-index); digit++ {
+	for digit := start; digit <= 9; digit++ {
+		// Avoid placing too large digits that would prevent full length n
+		if 10-digit < n-index {
+			break
+		}
 		comb[index] = digit
 		generateComb(comb, index+1, digit+1, n)
 	}
