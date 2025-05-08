c1c925c27bbee1bf49f52c797f7e54e5683ce248
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 14:02:44 +0300

    Rechecked divmod.go file

--- divmod.go
+++ divmod.go
@@ -1,6 +1,12 @@
+// divmod.go
 package student
 
 func DivMod(a int, b int, div *int, mod *int) {
-	*div = a / b
-	*mod = a % b
+	if b != 0 {
+		*div = a / b
+		*mod = a % b
+	} else {
+		*div = 0
+		*mod = 0
+	}
 }
