e8eeab0a4df97a236ac992a8098838a71fc83cc8
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 15:08:00 +0300

    Adding atoi.go correction

--- atoi.go
+++ atoi.go
@@ -2,11 +2,16 @@ package student
 
 func Atoi(s string) int {
 	result := 0
+	sign := 1
+	if len(s) > 0 && s[0] == '-' {
+		sign = -1
+		s = s[1:]
+	}
 	for _, char := range s {
 		if char < '0' || char > '9' {
 			return 0
 		}
 		result = result*10 + int(char-'0')
 	}
-	return result
+	return result * sign
 }
