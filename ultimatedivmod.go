60744147e8c871db58f101da631ad7da3380658c
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 13:59:50 +0300

    Adding ultimatedivmod.go correction 3

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,10 +1,11 @@
 package student
 
 func UltimateDivMod(a *int, b *int) {
-	if *b != 0 {
+	if b != nil && *b != 0 { // Ensure b is not nil and not zero
 		*a = *a / *b
 		*b = *a % *b
 	} else {
+		// Handle division by zero and set a and b to zero
 		*a = 0
 		*b = 0
 	}
