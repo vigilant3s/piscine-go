d333fd2cf045ca0b95cd6543edcdaccad6e36137
dgalanis <galanisdimitrios84@gmail.com>
2025-05-08 15:07:18 +0300

    Adding atoi.go file

---
+++ atoi.go
@@ -0,0 +1,12 @@
+package student
+
+func Atoi(s string) int {
+	result := 0
+	for _, char := range s {
+		if char < '0' || char > '9' {
+			return 0
+		}
+		result = result*10 + int(char-'0')
+	}
+	return result
+}
