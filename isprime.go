c1e19670bc3c295c3b6863c245c926e16861825f
Dimitris Galanis <dgalanis@noreply.platform.zone01.gr>
2025-05-13 12:21:43 +0000

    Add isprime.go

---
+++ isprime.go
@@ -0,0 +1,21 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	if nb == 2 || nb == 3 {
+		return true
+	}
+	if nb%2 == 0 || nb%3 == 0 {
+		return false
+	}
+
+	// Check from 5 to sqrt(nb) using 6-step
+	for i := 5; i*i <= nb; i += 6 {
+		if nb%i == 0 || nb%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
