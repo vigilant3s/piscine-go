f97db7a08f93e7edc2ed8881de3a725bb5c5b0f1
dgalanis <galanisdimitrios84@gmail.com>
2025-05-14 23:40:35 +0300

    Add non-cheating nbrconvertalpha solution and .gitignore

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -12,44 +12,37 @@ func Atoi(s string) int {
 	result := 0
 	for _, ch := range s {
 		if ch < '0' || ch > '9' {
-			return -1 // invalid input
+			return -1
 		}
 		result = result*10 + int(ch-'0')
 	}
 	return result
 }
 
-// PrintString prints a string using z01.PrintRune
-func PrintString(s string) {
-	for _, ch := range s {
-		z01.PrintRune(ch)
-	}
-}
-
 func main() {
 	args := os.Args[1:]
-
-	uppercase := false
-	startIndex := 0
-
-	// Check for --upper flag
-	if len(args) > 0 && args[0] == "--upper" {
-		uppercase = true
-		startIndex = 1
+	if len(args) == 0 {
+		return // nothing to print, not even a newline
 	}
 
-	for i := startIndex; i < len(args); i++ {
+	uppercase := false
+	start := 0
+
+	if args[0] == "--upper" {
+		uppercase = true
+		start = 1
+	}
+
+	for i := start; i < len(args); i++ {
 		n := Atoi(args[i])
 		if n < 1 || n > 26 {
 			z01.PrintRune(' ')
 			continue
 		}
-
 		if uppercase {
 			z01.PrintRune(rune('A' + n - 1))
 		} else {
 			z01.PrintRune(rune('a' + n - 1))
 		}
 	}
-	z01.PrintRune('\n')
 }
