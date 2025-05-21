b03e18e155c4437d6b9a8429b9edc98f500dfd96
dgalanis <galanisdimitrios84@gmail.com>
2025-05-21 17:08:00 +0300

    Adding Cat Code -fmt package

--- cat/main.go
+++ cat/main.go
@@ -1,32 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
 )
 
-func main() {
-	args := os.Args[1:]
+func main() { // cat.go
+	args := os.Args[1:] // Ignore the first argument (the program name)
 
-	// No arguments: read from stdin
-	if len(args) == 0 { // No arguments provided
-		_, err := io.Copy(os.Stdout, os.Stdin) // Read from stdin and write to stdout
+	// No arguments → read from stdin
+	if len(args) == 0 { // cat stdin
+		_, err := io.Copy(os.Stdout, os.Stdin) // Copy from stdin to stdout
 		if err != nil {                        // Handle error
-			fmt.Fprintln(os.Stderr, "ERROR:", err) // Print error message to stderr
-			os.Exit(1)                             // Exit with error code
+			os.Stderr.WriteString("ERROR: " + err.Error() + "\n") // Write error to stderr
+			os.Exit(1)                                            // Exit with error code
 		}
-		return // Exit after copying stdin to stdout
+		return // Exit if no arguments
 	}
 
-	// Arguments provided: treat as filenames
-	for _, filename := range args { // Iterate over each filename
+	// Loop over each argument
+	for _, filename := range args { // cat file1 file2 ...
 		file, err := os.Open(filename) // Open the file
 		if err != nil {                // Handle error
-			fmt.Fprintf(os.Stderr, "ERROR: %v\n", err) // Print error message to stderr
-			os.Exit(1)                                 // Exit with error code
+			os.Stderr.WriteString("ERROR: open " + filename + ": " + err.Error() + "\n") // Write error to stderr
+			os.Exit(1)                                                                   // Exit with error code
 		}
-		io.Copy(os.Stdout, file) // Copy file content to stdout
+		io.Copy(os.Stdout, file) // Copy from file to stdout
 		file.Close()             // Close the file
 	}
 }
