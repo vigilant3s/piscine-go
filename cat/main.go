d7be10e3416e4bfed46ecbd8a1ac98e840979ba1
dgalanis <galanisdimitrios84@gmail.com>
2025-05-21 17:09:22 +0300

    Cat v.02

--- cat/main.go
+++ cat/main.go
@@ -5,27 +5,25 @@ import (
 	"os"
 )
 
-func main() { // cat.go
-	args := os.Args[1:] // Ignore the first argument (the program name)
+func main() {
+	args := os.Args[1:]
 
-	// No arguments → read from stdin
-	if len(args) == 0 { // cat stdin
-		_, err := io.Copy(os.Stdout, os.Stdin) // Copy from stdin to stdout
-		if err != nil {                        // Handle error
-			os.Stderr.WriteString("ERROR: " + err.Error() + "\n") // Write error to stderr
-			os.Exit(1)                                            // Exit with error code
+	if len(args) == 0 {
+		_, err := io.Copy(os.Stdout, os.Stdin)
+		if err != nil {
+			os.Stderr.WriteString("ERROR: " + err.Error() + "\n")
+			os.Exit(1)
 		}
-		return // Exit if no arguments
+		return
 	}
 
-	// Loop over each argument
-	for _, filename := range args { // cat file1 file2 ...
-		file, err := os.Open(filename) // Open the file
-		if err != nil {                // Handle error
-			os.Stderr.WriteString("ERROR: open " + filename + ": " + err.Error() + "\n") // Write error to stderr
-			os.Exit(1)                                                                   // Exit with error code
+	for _, filename := range args {
+		file, err := os.Open(filename)
+		if err != nil {
+			os.Stderr.WriteString("ERROR: " + err.Error() + "\n")
+			os.Exit(1)
 		}
-		io.Copy(os.Stdout, file) // Copy from file to stdout
-		file.Close()             // Close the file
+		io.Copy(os.Stdout, file)
+		file.Close()
 	}
 }
