4c55247f98418bf61509826eb99b7b3f45f1bee3
dgalanis <galanisdimitrios84@gmail.com>
2025-05-22 20:09:19 +0300

    Adding String to Slice code

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,6 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
+	if str == "" {
+		return nil
+	}
 	result := []int{}
 	for _, char := range str {
 		result = append(result, int(char))
