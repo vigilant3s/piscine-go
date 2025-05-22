abcb10ecd526d8427727381410bdd999cc596ba7
dgalanis <galanisdimitrios84@gmail.com>
2025-05-22 18:51:10 +0300

    Adding Unmatch corrected Code v.3

--- unmatch.go
+++ unmatch.go
@@ -1,19 +1,18 @@
 package piscine
 
 func Unmatch(a []int) int {
-	freq := make(map[int]int) // Frequency map
+	freq := make(map[int]int) // Step 1: Count frequency of each number
 
-	// Count the occurrences of each number
 	for _, num := range a {
 		freq[num]++
 	}
 
-	// Find the number that occurs an odd number of times
-	for num, count := range freq {
-		if count%2 != 0 {
+	// Step 2: Loop in order to find the first odd-count number
+	for _, num := range a {
+		if freq[num]%2 != 0 {
 			return num
 		}
 	}
 
-	return -1
+	return -1 // All numbers had even count
 }
